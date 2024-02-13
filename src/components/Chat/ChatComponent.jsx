29984ea5eb15690a732fe60816dc3c43c0918433
azhararrozak <azhardrozak2001@upi.edu>
2024-02-13 16:12:37 +0700

    Fix socket io

--- src/components/Chat/ChatComponent.jsx
+++ src/components/Chat/ChatComponent.jsx
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import AuthService from "../../services/auth.service";
 import ChatService from "../../services/chat.service";
 
-const socket = io("http://localhost:5000");
+const socket = io(import.meta.env.VITE_BASE_API);
 
 const ChatComponent = () => {
   const [user, setUser] = useState(undefined);
