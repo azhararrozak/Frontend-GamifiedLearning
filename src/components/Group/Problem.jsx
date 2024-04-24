5b193e39e8d6e119b61c292b0fd17f2af9d8dd14
azhararrozak <azhardrozak2001@upi.edu>
2024-04-24 12:36:48 +0700

    fix pages list problem

--- src/components/Group/Problem.jsx
+++ src/components/Group/Problem.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import GroupService from '../../services/group.service';
+import React from "react";
+import { useParams } from "react-router-dom";
+import GroupService from "../../services/group.service";
 import { toast } from "react-hot-toast";
 
 const Problem = () => {
@@ -9,13 +9,33 @@ const Problem = () => {
   const [problems, setProblems] = React.useState([
     {
       id: 1,
-      title: 'Problem 1',
-      description: 'Description for Problem 1',
+      title: "Permasalahan 1 - Pertemuan 1",
+      description: "Description for Permasalahan 1",
     },
     {
       id: 2,
-      title: 'Problem 2',
-      description: 'Description for Problem 2',
+      title: "Permasalahan 2 - Pertemuan 1",
+      description: "Description for Permasalahan 2",
+    },
+    {
+      id: 3,
+      title: "Permasalahan 1 - Pertemuan 2",
+      description: "Description for Permasalahan 2",
+    },
+    {
+      id: 4,
+      title: "Permasalahan 2 - Pertemuan 2",
+      description: "Description for Permasalahan 2",
+    },
+    {
+      id: 5,
+      title: "Permasalahan 1 - Pertemuan 3",
+      description: "Description for Permasalahan 2",
+    },
+    {
+      id: 5,
+      title: "Permasalahan 2 - Pertemuan 3",
+      description: "Description for Permasalahan 2",
     },
   ]);
 
@@ -25,7 +45,11 @@ const Problem = () => {
     //Send to server
     try {
       const { title, description } = selectedProblem;
-      const response = await GroupService.updateProblem(groupId, title, description);
+      const response = await GroupService.updateProblem(
+        groupId,
+        title,
+        description
+      );
       toast.success(response.data.message);
     } catch (error) {
       console.log(error);
@@ -33,21 +57,25 @@ const Problem = () => {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="grid grid-cols sm:grid-cols-2 lg:grid-cols-4 gap-4 my-4">
       {problems.map((problem) => (
-        <div key={problem.id} className="max-w-md mx-4 bg-white p-6 rounded-md shadow-md">
-          <img src='./home.png' />
-          <h1 className="text-xl font-semibold mb-4">{problem.title}</h1>
-          <p className="text-gray-600">{problem.description}</p>
-          <button
-            onClick={() => handleSaveProblem(problem)}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-4"
-          >
-            Pilih Problem Ini
-          </button>
+        <div
+          key={problem.id}
+          className="max-w-md mx-4 bg-white rounded-md shadow-md"
+        >
+          <img className="rounded" src="../../ProblemImage.jpg" />
+          <div className="p-4">
+            <h1 className="text-xl font-semibold mb-2">{problem.title}</h1>
+            <p className="text-gray-600">{problem.description}</p>
+            <button
+              onClick={() => handleSaveProblem(problem)}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-4"
+            >
+              Pilih Permasalahan Ini
+            </button>
+          </div>
         </div>
       ))}
-      
     </div>
   );
 };
