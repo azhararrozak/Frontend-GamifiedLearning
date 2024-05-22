4fdd41dd39d0c7b547742878d36a99e0de9a047e
azhararrozak <azhardrozak2001@upi.edu>
2024-05-22 20:01:27 +0700

    Add indicator Logical thinking in HasilNilai

--- src/components/HasilNilai/HasilNilaiPostest.jsx
+++ src/components/HasilNilai/HasilNilaiPostest.jsx
@@ -29,7 +29,7 @@ const HasilNilaiPostest = () => {
             {answer &&
               answer.map((soal, index) => (
                 <th key={index} className="border border-secondary">
-                  Soal {index + 1} <span>({soal.indicator})</span>
+                  S{index + 1} {/*<span>({soal.indicator})</span>*/}
                 </th>
               ))}
             <th className="border border-secondary">Soal Benar</th>
@@ -52,6 +52,109 @@ const HasilNilaiPostest = () => {
         </tbody>
       </table>
       </div>
+
+      <div className="p-3">
+      <h1 className="text-center text-2xl my-4 font-bold"> Berdasarkan Indikator Penarikan Kesimpulan</h1>
+      <table className="w-full border-2 border-secondary">
+        <thead>
+          <tr>
+            <th className="border border-secondary">No</th>
+            <th className="border border-secondary"> Nama Siswa</th>
+            {answer &&
+              answer.map((soal, index) => (
+                <th key={index} className="border border-secondary">
+                  S{index + 1} {/*<span>({soal.indicator})</span>*/}
+                </th>
+              ))}
+            <th className="border border-secondary">Soal Benar</th>
+          </tr>
+        </thead>
+        <tbody className="text-center">
+          {nilaiPostest &&
+            nilaiPostest.map((jawaban, index) => (
+              <tr key={index}>
+                <td className="border border-secondary">{index + 1}</td>
+                <td className="border border-secondary">{jawaban.user.username}</td>
+                {jawaban.answers.filter(a => a.indicator === "Penarikan Kesimpulan").map((soal, index) => (
+                  <td key={index} className="border border-secondary">{soal.isCorrect ? "1" : "0"}</td>
+                ))}
+                <td className="border border-secondary">
+                  {jawaban.answers.filter(a => a.indicator === "Penarikan Kesimpulan").filter((soal) => soal.isCorrect).length}
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+      </div>
+
+      <div className="p-3">
+      <h1 className="text-center text-2xl my-4 font-bold"> Berdasarkan Indikator Keruntutan Berpikir</h1>
+      <table className="w-full border-2 border-secondary">
+        <thead>
+          <tr>
+            <th className="border border-secondary">No</th>
+            <th className="border border-secondary"> Nama Siswa</th>
+            {answer &&
+              answer.map((soal, index) => (
+                <th key={index} className="border border-secondary">
+                  S{index + 1} {/*<span>({soal.indicator})</span>*/}
+                </th>
+              ))}
+            <th className="border border-secondary">Soal Benar</th>
+          </tr>
+        </thead>
+        <tbody className="text-center">
+          {nilaiPostest &&
+            nilaiPostest.map((jawaban, index) => (
+              <tr key={index}>
+                <td className="border border-secondary">{index + 1}</td>
+                <td className="border border-secondary">{jawaban.user.username}</td>
+                {jawaban.answers.filter(a => a.indicator === "Keruntutan Berpikir").map((soal, index) => (
+                  <td key={index} className="border border-secondary">{soal.isCorrect ? "1" : "0"}</td>
+                ))}
+                <td className="border border-secondary">
+                  {jawaban.answers.filter(a => a.indicator === "Keruntutan Berpikir").filter((soal) => soal.isCorrect).length}
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+      </div>
+
+      <div className="p-3">
+      <h1 className="text-center text-2xl my-4 font-bold"> Berdasarkan Indikator Kemampuan Berargumen</h1>
+      <table className="w-full border-2 border-secondary">
+        <thead>
+          <tr>
+            <th className="border border-secondary">No</th>
+            <th className="border border-secondary"> Nama Siswa</th>
+            {answer &&
+              answer.map((soal, index) => (
+                <th key={index} className="border border-secondary">
+                  S{index + 1} {/*<span>({soal.indicator})</span>*/}
+                </th>
+              ))}
+            <th className="border border-secondary">Soal Benar</th>
+          </tr>
+        </thead>
+        <tbody className="text-center">
+          {nilaiPostest &&
+            nilaiPostest.map((jawaban, index) => (
+              <tr key={index}>
+                <td className="border border-secondary">{index + 1}</td>
+                <td className="border border-secondary">{jawaban.user.username}</td>
+                {jawaban.answers.filter(a => a.indicator === "Kemampuan Berargumen").map((soal, index) => (
+                  <td key={index} className="border border-secondary">{soal.isCorrect ? "1" : "0"}</td>
+                ))}
+                <td className="border border-secondary">
+                  {jawaban.answers.filter(a => a.indicator === "Kemampuan Berargumen").filter((soal) => soal.isCorrect).length}
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+      </div>
+
     </div>
   );
 };
--- src/components/HasilNilai/HasilNilaiPretest.jsx
+++ src/components/HasilNilai/HasilNilaiPretest.jsx
@@ -16,6 +16,9 @@ const HasilNilaiPretest = () => {
       });
   }, []);
 
+  //console.log(nilaiPretest);
+
+
   return (
     <div>
       <h1 className="text-center text-2xl my-4 font-bold">
@@ -34,7 +37,7 @@ const HasilNilaiPretest = () => {
                   S{index + 1} {/*<span>({soal.indicator})</span>*/}
                 </th>
               ))}
-            {/* <th className="border border-secondary">Soal Benar</th> */}
+            <th className="border border-secondary">Soal Benar</th>
           </tr>
         </thead>
         <tbody className="text-center">
@@ -46,14 +49,117 @@ const HasilNilaiPretest = () => {
                 {jawaban.answers.map((soal, index) => (
                   <td key={index} className="border border-secondary">{soal.isCorrect ? "1" : "0"}</td>
                 ))}
-                {/* <td className="border border-secondary">
+                <td className="border border-secondary">
                   {jawaban.answers.filter((soal) => soal.isCorrect).length}
-                </td> */}
+                </td>
               </tr>
             ))}
         </tbody>
       </table>
       </div>
+
+      <div className="p-3">
+      <h1 className="text-center text-2xl my-4 font-bold"> Berdasarkan Indikator Penarikan Kesimpulan</h1>
+      <table className="w-full border-2 border-secondary">
+        <thead>
+          <tr>
+            <th className="border border-secondary">No</th>
+            <th className="border border-secondary"> Nama Siswa</th>
+            {answer &&
+              answer.filter(a => a.indicator === "Penarikan Kesimpulan").map((soal, index) => (
+                <th key={index} className="border border-secondary">
+                  S{index + 1} {/*<span>({soal.indicator})</span>*/}
+                </th>
+              ))}
+            <th className="border border-secondary">Soal Benar</th>
+          </tr>
+        </thead>
+        <tbody className="text-center">
+          {nilaiPretest &&
+            nilaiPretest.map((jawaban, index) => (
+              <tr key={index}>
+                <td className="border border-secondary">{index + 1}</td>
+                <td className="border border-secondary">R{index + 1}</td>
+                {jawaban.answers.filter(a => a.indicator === "Penarikan Kesimpulan").map((soal, index) => (
+                  <td key={index} className="border border-secondary">{soal.isCorrect ? "1" : "0"}</td>
+                ))}
+                <td className="border border-secondary">
+                  {jawaban.answers.filter(a => a.indicator === "Penarikan Kesimpulan").filter((soal) => soal.isCorrect).length}
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+      </div>
+
+      <div className="p-3">
+      <h1 className="text-center text-2xl my-4 font-bold"> Berdasarkan Indikator Keruntutan Berpikir</h1>
+      <table className="w-full border-2 border-secondary">
+        <thead>
+          <tr>
+            <th className="border border-secondary">No</th>
+            <th className="border border-secondary"> Nama Siswa</th>
+            {answer &&
+              answer.filter(a => a.indicator === "Keruntutan Berpikir").map((soal, index) => (
+                <th key={index} className="border border-secondary">
+                  S{index + 1} {/*<span>({soal.indicator})</span>*/}
+                </th>
+              ))}
+            <th className="border border-secondary">Soal Benar</th>
+          </tr>
+        </thead>
+        <tbody className="text-center">
+          {nilaiPretest &&
+            nilaiPretest.map((jawaban, index) => (
+              <tr key={index}>
+                <td className="border border-secondary">{index + 1}</td>
+                <td className="border border-secondary">R{index + 1}</td>
+                {jawaban.answers.filter(a => a.indicator === "Keruntutan Berpikir").map((soal, index) => (
+                  <td key={index} className="border border-secondary">{soal.isCorrect ? "1" : "0"}</td>
+                ))}
+                <td className="border border-secondary">
+                  {jawaban.answers.filter(a => a.indicator === "Keruntutan Berpikir").filter((soal) => soal.isCorrect).length}
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+      </div>
+
+      <div className="p-3">
+      <h1 className="text-center text-2xl my-4 font-bold"> Berdasarkan Indikator Kemampuan Berargumen</h1>
+      <table className="w-full border-2 border-secondary">
+        <thead>
+          <tr>
+            <th className="border border-secondary">No</th>
+            <th className="border border-secondary"> Nama Siswa</th>
+            {answer &&
+              answer.filter(a => a.indicator === "Kemampuan Berargumen").map((soal, index) => (
+                <th key={index} className="border border-secondary">
+                  S{index + 1} {/*<span>({soal.indicator})</span>*/}
+                </th>
+              ))}
+            <th className="border border-secondary">Soal Benar</th>
+          </tr>
+        </thead>
+        <tbody className="text-center">
+          {nilaiPretest &&
+            nilaiPretest.map((jawaban, index) => (
+              <tr key={index}>
+                <td className="border border-secondary">{index + 1}</td>
+                <td className="border border-secondary">R{index + 1}</td>
+                {jawaban.answers.filter(a => a.indicator === "Kemampuan Berargumen").map((soal, index) => (
+                  <td key={index} className="border border-secondary">{soal.isCorrect ? "1" : "0"}</td>
+                ))}
+                <td className="border border-secondary">
+                  {jawaban.answers.filter(a => a.indicator === "Kemampuan Berargumen").filter((soal) => soal.isCorrect).length}
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+      </div>
+
     </div>
   );
 };
